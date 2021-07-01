abd182e2f706f31a32afc170be556650cce6af39
wbadmin <>
2021-07-01 16:38:10 +0300

    {/src/main/java/com/myspace/encyclopediaofdrugs/article.java}

--- src/main/java/com/myspace/encyclopediaofdrugs/article.java
+++ src/main/java/com/myspace/encyclopediaofdrugs/article.java
@@ -6,12 +6,91 @@ package com.myspace.encyclopediaofdrugs;
 
 public class article implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public article() {
-    }
+	private com.myspace.encyclopediaofdrugs.Medicament medicament;
+	private com.myspace.encyclopediaofdrugs.MedicamentFormula formula;
+	private com.myspace.encyclopediaofdrugs.MedicamentGroup group;
+	private com.myspace.encyclopediaofdrugs.MedicamentInterract interract;
+	private com.myspace.encyclopediaofdrugs.MedicamentType type;
+	private java.lang.String author;
+	private java.time.OffsetDateTime dateChange;
 
+	public article() {
+	}
 
+	public com.myspace.encyclopediaofdrugs.Medicament getMedicament() {
+		return this.medicament;
+	}
 
+	public void setMedicament(
+			com.myspace.encyclopediaofdrugs.Medicament medicament) {
+		this.medicament = medicament;
+	}
+
+	public com.myspace.encyclopediaofdrugs.MedicamentFormula getFormula() {
+		return this.formula;
+	}
+
+	public void setFormula(
+			com.myspace.encyclopediaofdrugs.MedicamentFormula formula) {
+		this.formula = formula;
+	}
+
+	public com.myspace.encyclopediaofdrugs.MedicamentGroup getGroup() {
+		return this.group;
+	}
+
+	public void setGroup(com.myspace.encyclopediaofdrugs.MedicamentGroup group) {
+		this.group = group;
+	}
+
+	public com.myspace.encyclopediaofdrugs.MedicamentInterract getInterract() {
+		return this.interract;
+	}
+
+	public void setInterract(
+			com.myspace.encyclopediaofdrugs.MedicamentInterract interract) {
+		this.interract = interract;
+	}
+
+	public com.myspace.encyclopediaofdrugs.MedicamentType getType() {
+		return this.type;
+	}
+
+	public void setType(com.myspace.encyclopediaofdrugs.MedicamentType type) {
+		this.type = type;
+	}
+
+	public java.lang.String getAuthor() {
+		return this.author;
+	}
+
+	public void setAuthor(java.lang.String author) {
+		this.author = author;
+	}
+
+	public java.time.OffsetDateTime getDateChange() {
+		return this.dateChange;
+	}
+
+	public void setDateChange(java.time.OffsetDateTime dateChange) {
+		this.dateChange = dateChange;
+	}
+
+	public article(com.myspace.encyclopediaofdrugs.Medicament medicament,
+			com.myspace.encyclopediaofdrugs.MedicamentFormula formula,
+			com.myspace.encyclopediaofdrugs.MedicamentGroup group,
+			com.myspace.encyclopediaofdrugs.MedicamentInterract interract,
+			com.myspace.encyclopediaofdrugs.MedicamentType type,
+			java.lang.String author, java.time.OffsetDateTime dateChange) {
+		this.medicament = medicament;
+		this.formula = formula;
+		this.group = group;
+		this.interract = interract;
+		this.type = type;
+		this.author = author;
+		this.dateChange = dateChange;
+	}
 
 }
