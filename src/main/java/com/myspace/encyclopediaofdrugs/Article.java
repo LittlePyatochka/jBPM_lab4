e29fcca8b56e197f74ad12e5a69f4b093b807e1a
wbadmin <>
2021-07-02 02:56:27 +0300

    {/src/main/java/com/myspace/encyclopediaofdrugs/Article.java}

--- src/main/java/com/myspace/encyclopediaofdrugs/Article.java
+++ src/main/java/com/myspace/encyclopediaofdrugs/Article.java
@@ -8,11 +8,6 @@ public class Article implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	private com.myspace.encyclopediaofdrugs.Medicament medicament;
-	private com.myspace.encyclopediaofdrugs.MedicamentFormula formula;
-	private com.myspace.encyclopediaofdrugs.MedicamentGroup group;
-	private com.myspace.encyclopediaofdrugs.MedicamentInterract interract;
-	private com.myspace.encyclopediaofdrugs.MedicamentType type;
 	private java.time.OffsetDateTime dateChange;
 
 	private java.lang.String price;
@@ -24,49 +19,6 @@ public class Article implements java.io.Serializable {
 	private java.lang.String title;
 
 	public Article() {
-	}
-
-	public com.myspace.encyclopediaofdrugs.Medicament getMedicament() {
-		return this.medicament;
-	}
-
-	public void setMedicament(
-			com.myspace.encyclopediaofdrugs.Medicament medicament) {
-		this.medicament = medicament;
-	}
-
-	public com.myspace.encyclopediaofdrugs.MedicamentFormula getFormula() {
-		return this.formula;
-	}
-
-	public void setFormula(
-			com.myspace.encyclopediaofdrugs.MedicamentFormula formula) {
-		this.formula = formula;
-	}
-
-	public com.myspace.encyclopediaofdrugs.MedicamentGroup getGroup() {
-		return this.group;
-	}
-
-	public void setGroup(com.myspace.encyclopediaofdrugs.MedicamentGroup group) {
-		this.group = group;
-	}
-
-	public com.myspace.encyclopediaofdrugs.MedicamentInterract getInterract() {
-		return this.interract;
-	}
-
-	public void setInterract(
-			com.myspace.encyclopediaofdrugs.MedicamentInterract interract) {
-		this.interract = interract;
-	}
-
-	public com.myspace.encyclopediaofdrugs.MedicamentType getType() {
-		return this.type;
-	}
-
-	public void setType(com.myspace.encyclopediaofdrugs.MedicamentType type) {
-		this.type = type;
 	}
 
 	public java.time.OffsetDateTime getDateChange() {
@@ -109,19 +61,9 @@ public class Article implements java.io.Serializable {
 		this.title = title;
 	}
 
-	public Article(com.myspace.encyclopediaofdrugs.Medicament medicament,
-			com.myspace.encyclopediaofdrugs.MedicamentFormula formula,
-			com.myspace.encyclopediaofdrugs.MedicamentGroup group,
-			com.myspace.encyclopediaofdrugs.MedicamentInterract interract,
-			com.myspace.encyclopediaofdrugs.MedicamentType type,
-			java.time.OffsetDateTime dateChange, java.lang.String price,
+	public Article(java.time.OffsetDateTime dateChange, java.lang.String price,
 			java.util.List<java.lang.String> links,
 			java.lang.Boolean isPublished, java.lang.String title) {
-		this.medicament = medicament;
-		this.formula = formula;
-		this.group = group;
-		this.interract = interract;
-		this.type = type;
 		this.dateChange = dateChange;
 		this.price = price;
 		this.links = links;
